09acb0ff898ccaa1e36c2ba3faabbb53a559fc70
Johan Jaxing <johan.jaxing@gmail.com>
2022-04-14 15:46:50 +0200

    Add configs for mulitplte runs

---
+++ configs/_base_/datasets/nus-3d-6sweep.py
@@ -0,0 +1,19 @@
+_base_=["./nus-3d-1sweep.py"]
+
+number_of_sweeps = 5  # Extra sweeps to be merged. Max is 6 for now.
+train_pipeline = [
+    dict(
+        type='LoadPointsFromMultiSweeps',
+        sweeps_num=number_of_sweeps,),
+]
+test_pipeline = [
+    dict(
+        type='LoadPointsFromMultiSweeps',
+        sweeps_num=number_of_sweeps,),
+]
+eval_pipeline = [
+    dict(
+        type='LoadPointsFromMultiSweeps',
+        sweeps_num=number_of_sweeps,
+    )
+]
