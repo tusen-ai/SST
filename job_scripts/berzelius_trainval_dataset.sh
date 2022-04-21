2594b116158fdac590dae9aa619c64d7237eb55b
eliassv <eliassv@student.chalmers.se>
2022-04-21 17:12:31 +0200

    Fixed sbatch script for berzelius

--- job_scripts/berzelius_trainval_dataset.sh
+++ job_scripts/berzelius_trainval_dataset.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
-#SBATCH -A SNIC2021-7-127 -p alvis
-#SBATCH -t 16:00:00
-#SBATCH --gpus-per-node=A40:4
+#!/usr/bin/env bash
+#SBATCH -t 0-16:00:00
+#SBATCH --gpus=6
 #SBATCH -N 1
-#SBATCH --output=/cephyr/NOBACKUP/groups/snic2021-7-127/eliassv/slurm-out/slurm-%j.out
-#SBATCH -J "Some job name"  # multi node, multi GPU
+#SBATCH --output=/proj/deep-mot/eliassv/slurm-out/slurm-%j.out
+#SBATCH -J "Some job name"  # single node, multi GPU
 CONFIG=${1:-sst_nuscenesD5_1x_3class_8heads_v2}
 REPO_NUMBER=${2:-1}  # Choose between repo 1 or 2
 echo $CONFIG
-GPUS_PER_NODE=4
-export GPU_TYPE=A40
-# Options for GPU type are T4 or A40. Choose A40 also when running on V100, A100 or A100fat.
+GPUS_PER_NODE=6  # Max is 8 per mode
 export OMP_NUM_THREADS=16  # cores per gpu (16 cores per A40)
-
 
 echo $HOSTNAME
 echo $SLURM_JOB_NODELIST
@@ -21,48 +18,20 @@ export MASTER_ADDR=$HOSTNAME
 export MASTER_PORT=12345
 export PORT=$MASTER_PORT
 export JOB_ID=$SLURM_JOB_ID
-export NGPUS_PER_NODE=$(echo "$SLURM_GPUS_PER_NODE" | sed 's/[A-Z0-9]*:\([0-9]*\)*/\1/')
 
 echo ""
 echo "This job $JOB_ID was started as:
   bash tools/dist_train.sh configs/sst_refactor/$CONFIG.py $GPUS_PER_NODE \
-    --work-dir /cephyr/NOBACKUP/groups/snic2021-7-127/eliassv/jobs/$JOB_ID \
+    --work-dir /proj/deep-mot/eliassv/jobs/$JOB_ID \
     --cfg-options evaluation.metric=nuscenes" ${@:3}
 echo ""
 
-echo ""
-echo "Start copying repo to '$TMPDIR'"
-if [$REPO_NUMBER == 1]
-then
-   cp -r /cephyr/NOBACKUP/groups/snic2021-7-127/eliassv/SST_${GPU_TYPE}/ $TMPDIR/SST_${GPU_TYPE}
-else
-   cp -r /cephyr/NOBACKUP/groups/snic2021-7-127/eliassv/SST_${GPU_TYPE}_2/ $TMPDIR/SST_${GPU_TYPE}
-fi
-echo ""
-echo "Copying of repo to tempdir is now done."
-echo ""
-
-echo ""
-echo "Start copying nusenes info to '$TMPDIR/SST_$GPU_TYPE/'"
-unzip -q -d $TMPDIR/SST_$GPU_TYPE /cephyr/NOBACKUP/groups/snic2021-7-127/eliassv/nuscenes_info/nuscenes_infos_v2.zip
-echo ""
-echo "Copying of nusenes info to repo in tempdir is now done."
-echo ""
-
-echo ""
-echo "Start copying dataset to '$TMPDIR/SST_$GPU_TYPE/'"
-source ./unpack_trainval.sh
-echo ""
-echo "Copying of dataset to repo in tempdir is now done."
-echo ""
-
-cd $TMPDIR/SST_$GPU_TYPE
-singularity exec --pwd $TMPDIR/SST_$GPU_TYPE \
-  /cephyr/NOBACKUP/groups/snic2021-7-127/eliassv/sst_env/mmdetection3d_$GPU_TYPE.sif \
+singularity exec --pwd /proj/deep-mot/eliassv/SST_${REPO_NUMBER} \
+  /proj/deep-mot/eliassv/sst_env/mmdetection3d_A40.sif \
   bash tools/dist_train.sh configs/sst_refactor/$CONFIG.py $GPUS_PER_NODE \
-  --work-dir /cephyr/NOBACKUP/groups/snic2021-7-127/eliassv/jobs/$JOB_ID \
+  --work-dir /proj/deep-mot/eliassv/jobs/$JOB_ID \
   --cfg-options evaluation.metric=nuscenes ${@:3}
 # ${@:3} grabs everything after the third argument these are used to overwrite settings in the config file
 # e.g. load_from="config/path/epoch_n.pth" loads the weights from the provided model
 
-cp /cephyr/NOBACKUP/groups/snic2021-7-127/eliassv/slurm-out/slurm-$JOB_ID.out /cephyr/NOBACKUP/groups/snic2021-7-127/eliassv/jobs/$JOB_ID
+cp /proj/deep-mot/eliassv/slurm-out/slurm-$JOB_ID.out /proj/deep-mot/eliassv/jobs/$JOB_ID
