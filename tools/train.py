5f31b5fe245e567684cab96550b3dca1932b24d3
Lue Fan <1060056270@qq.com>
2022-09-20 12:08:49 +0800

    default seed 1

--- tools/train.py
+++ tools/train.py
@@ -45,7 +45,7 @@ def parse_args():
         nargs='+',
         help='ids of gpus to use '
         '(only applicable to non-distributed training)')
-    parser.add_argument('--seed', type=int, default=0, help='random seed')
+    parser.add_argument('--seed', type=int, default=1, help='random seed')
     parser.add_argument('--log-name', type=str, default='', help='random seed')
     parser.add_argument(
         '--deterministic',
