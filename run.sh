6d8dd69c41476b813ef9bf57bdb40f3208615c6b
Abyssaledge <1060056270@qq.com>
2022-09-23 05:54:39 +0000

    update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 DIR=fsd
 WORK=work_dirs
 CONFIG=fsd_waymoD1_1x
-bash tools/dist_train.sh configs/$DIR/$CONFIG.py 8 --work-dir ./$WORK/$CONFIG/ --cfg-options evaluation.pklfile_prefix=./$WORK/$CONFIG/results evaluation.metric=waymo --seed 1
+bash tools/dist_train.sh configs/$DIR/$CONFIG.py 8 --work-dir ./$WORK/$CONFIG/ --cfg-options evaluation.pklfile_prefix=./$WORK/$CONFIG/results evaluation.metric=fast --seed 1
 
 # fast Waymo Evaluation
 # bash tools/dist_test.sh configs/$DIR/$CONFIG.py ./$WORK/$CONFIG/latest.pth 8 --options "pklfile_prefix=./$WORK/$CONFIG/fast_results" --eval fast
