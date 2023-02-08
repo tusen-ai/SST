64ac60b95662fb8b1a0258bc94b4c30c853f65ff
Abyssaledge <1060056270@qq.com>
2023-02-08 06:51:22 +0000

    add jump config

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 DIR=fsd
 WORK=work_dirs
-CONFIG=fsd_waymoD1_1x_3f_multisweeps
+CONFIG=fsd_waymoD1_1x_3f_jumpsweeps
 bash tools/dist_train.sh configs/$DIR/$CONFIG.py 8 --work-dir ./$WORK/$CONFIG/ --cfg-options evaluation.pklfile_prefix=./$WORK/$CONFIG/results evaluation.metric=fast --seed 1
 
 # fast Waymo Evaluation
