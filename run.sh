bb913ed6b80c50dbd8bfe40526fed5094ed8edea
Abyssaledge <1060056270@qq.com>
2023-02-08 09:27:40 +0000

    restore run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 DIR=fsd
 WORK=work_dirs
-CONFIG=fsd_waymoD1_1x_3f_futuresweeps
-# bash tools/dist_train.sh configs/$DIR/$CONFIG.py 8 --work-dir ./$WORK/$CONFIG/ --cfg-options evaluation.pklfile_prefix=./$WORK/$CONFIG/results evaluation.metric=fast --seed 1
+CONFIG=fsd_waymoD1_1x
+bash tools/dist_train.sh configs/$DIR/$CONFIG.py 8 --work-dir ./$WORK/$CONFIG/ --cfg-options evaluation.pklfile_prefix=./$WORK/$CONFIG/results evaluation.metric=fast --seed 1
 
 # fast Waymo Evaluation
-bash tools/dist_test.sh configs/$DIR/$CONFIG.py ./$WORK/$CONFIG/latest.pth 8 --options "pklfile_prefix=./$WORK/$CONFIG/fast_results" --eval fast
+# bash tools/dist_test.sh configs/$DIR/$CONFIG.py ./$WORK/$CONFIG/latest.pth 8 --options "pklfile_prefix=./$WORK/$CONFIG/fast_results" --eval fast
